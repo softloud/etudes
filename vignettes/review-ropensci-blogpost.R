32fe4d0fb793c75b4fe047b953feee64a0dd577a
Charles T. Gray <C.Gray@latrobe.edu.au>
2017-12-18 17:22:07 +1100

    Making progress on the review on the blog. :notes:

---
+++ vignettes/review-ropensci-blogpost.R
@@ -0,0 +1,10 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+# devtools::install_github("benmarwick/wordcountaddin", type = "source", dependencies = TRUE)
+library(wordcountaddin)
+
+
